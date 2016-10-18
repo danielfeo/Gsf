70eaed4340a571edeb8af43affd3bf1f877a1eb7
daniel feo <danielandres.feo@gmail.com>
2016-10-17 21:19:18 -0500

    conexion local

--- modelo/bd.class.php
+++ modelo/bd.class.php
@@ -6,10 +6,10 @@ Class Db{
 	public function conexion(){
 
 
-    	$conection['server']="mysql.hostinger.ph";  
-		$conection['user']="u686165894_gsf";       
-		$conection['pass']=")/daniel";        
-		$conection['base']="u686165894_gsf";       
+    	$conection['server']="127.0.0.1";  
+		$conection['user']="root";       
+		$conection['pass']="";        
+		$conection['base']="gestor_solicitudes";       
 		
 		$conect= mysqli_connect($conection['server'],$conection['user'],$conection['pass'],$conection['base']);
 		mysqli_query($conect,"SET NAMES 'utf8'");
